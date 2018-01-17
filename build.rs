7a2aa44206d63b6407f44438a4056d1da932bebd
Gleb Pomykalov <gleb@pomykalov.ru>
2018-01-17 21:59:18 +0300

    Use path to generate paths to C files

--- build.rs
+++ build.rs
@@ -1,20 +1,24 @@
 extern crate cc;
 
 use std::env;
+use std::path::Path;
 
 fn main() {
     let target = env::var("TARGET").unwrap();
     let target_os = target.split('-').nth(2).unwrap();
 
+    let c_path = Path::new("c");
+
     let mut builder = cc::Build::new();
-    match target_os {
-        "linux" => builder.file("c/linux.c"),
-        "darwin" => builder.file("c/macos.c"),
+    let c_file= match target_os {
+        "linux" => "linux.c",
+        "darwin" => "macos.c",
         "windows" => {
             println!("cargo:rustc-flags=-l psapi");
-            builder.file("c/windows.c")
+            "windows.c"
         },
         _ => panic!("Unsupported system")
     };
+    builder.file(c_path.join(c_file).to_str().unwrap());
     builder.compile("info");
 }
